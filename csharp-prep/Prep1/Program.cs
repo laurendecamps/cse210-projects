8580c294009f205406ed4289aafad55e9c350d4f
laurendecamps <131840329+laurendecamps@users.noreply.github.com>
2024-04-27 17:49:48 -0600

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first = console.readline();
+
+        Console.Write("What is your last name?")
+        Strinng last = console.readline();
+
+        console.writeline($"Your name is {last},{first} {last}.")
     }
 }
