bebfc35d1d2d747270a5d95f274873884dae1f17
laurendecamps <131840329+laurendecamps@users.noreply.github.com>
2024-04-27 17:56:53 -0600

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string answer = console.readline();
+        int percent = int.parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        { letter = "A"; }
+        else if (percent >= 80)
+        { letter = "B"; }
+        else if (percent >= 70)
+        { letter = "C"; }
+        else if (percent >= 60)
+        { letter = "D"; }
+        else { letter = "F"; }
+
+        console.writeline($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        { console.writeline("You passed!");}
+        else { console.writeline("Better luck next time!";)}
     }
 }
