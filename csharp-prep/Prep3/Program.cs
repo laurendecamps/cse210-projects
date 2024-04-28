d33bb1adf7912e374091214abb133cbfc91efb4f
laurendecamps <131840329+laurendecamps@users.noreply.github.com>
2024-04-27 18:03:33 -0600

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomgenerator = new Random();
+        int magicnumber = randomgenerator.next(1,101);
+
+        int guess = -1;
+
+        while (guess != magicnumber)
+    {
+
+        Console.WriteLine("What is your guess?");
+        guess = int.parse(console.readline());
+
+        if (magicnumber > guess)
+        { console.writeline("Higher");
+        }
+        else if (magicnumber < guess)
+        { console.writeline("Lower");
+        }
+        else
+        { console.writeline("You guessed it!");
+        }
+    }
     }
 }
